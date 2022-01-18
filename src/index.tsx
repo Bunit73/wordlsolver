2844251017c1840c1ceebb65ead012117d2c18a2
Ben <ford2bm@gmail.com>
2022-01-18 18:17:47 +0000

    Made word filter a python script instead of bash.

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 // Import the functions you need from the SDKs you need
-// import {initializeApp} from "firebase/app";
-// import {getAnalytics} from "firebase/analytics";
+import {initializeApp} from "firebase/app";
+import {getAnalytics} from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -23,8 +23,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 ReactDOM.render(
     <React.StrictMode>
---
+++ utils/word_filter.py
@@ -0,0 +1,20 @@
+filtered_words_file = open("filtered_words.txt","w")
+
+word_file = open("all_words.txt", 'r')
+words = word_file.readlines()
+
+def word_has_dup_char(word):
+    if len("".join(set(word))) == len(word):
+        return False
+    else:
+        return True
+
+for w in words:
+    w = w.strip().lower()
+
+    if len(w) == 5 and not word_has_dup_char(w) and w.isalpha():
+        print(w, file=filtered_words_file)
+    else:
+        continue
+
+
