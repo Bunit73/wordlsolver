bc1fd4e50eb7b1dc3b0bc6c3f520d1ec2058b8d1
Ben Ford <ford2bm@gmail.com>
2022-02-04 16:56:23 -0700

    Included letters can contain multiple letters

--- src/IncludedLetter.tsx
+++ src/IncludedLetter.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import {Box, Container, Form, Heading, Section} from "react-bulma-components";
-import {IIncludedLetter} from "./Models/Models";
+import {IIncludedLetters} from "./Models/Models";
+
+const unrepeated = (str: string): string => {
+    return Array.from(new Set(str)).join('')
+};
+
 
 function IncludedLetters(props: {
-    includedLetters: IIncludedLetter[],
-    onChange: (val: IIncludedLetter[]) => void;
+    includedLetters: IIncludedLetters[],
+    onChange: (val: IIncludedLetters[]) => void;
 }) {
     return <Section>
         <Container>
@@ -17,6 +22,7 @@ function IncludedLetters(props: {
                         props.includedLetters.map((l, i) => {
                             return <IncludedLetter
                                 key={i}
+                                placeHolderText={`Position ${(i + 1).toString()}`}
                                 includedLetter={l}
                                 onChange={(val) => {
                                     const ils = props.includedLetters;
@@ -34,39 +40,57 @@ function IncludedLetters(props: {
 }
 
 function IncludedLetter(props: {
-    includedLetter: IIncludedLetter;
-    onChange: (val: IIncludedLetter) => void;
+    includedLetter: IIncludedLetters;
+    onChange: (val: IIncludedLetters) => void;
+    placeHolderText: string;
 }) {
 
 
-    return <><Form.Field>
-        <Form.Control>
-            <Form.Input
-                maxLength={1}
-                onChange={(e) => {
-                    const il = props.includedLetter;
-                    il.letter = e.target.value;
-                    props.onChange(il);
-                }}
-            />
-        </Form.Control>
-
-    </Form.Field>
+    return <>
         <Form.Field>
             <Form.Control>
+                <Form.Input
+                    value={props.includedLetter.letters}
+                    placeholder={props.placeHolderText}
+                    onChange={(e) => {
+                        const val = e.target.value.replaceAll(' ', '').toLowerCase();
+                        const il = props.includedLetter;
+                        il.letters = unrepeated(val);
+                        props.onChange(il);
+                    }}
+                />
+            </Form.Control>
+
+        </Form.Field>
+        <Form.Field>
+            <Form.Control style={CheckBoxStyle}>
                 <Form.Checkbox
                     checked={props.includedLetter.correctPos}
                     onChange={(e) => {
                         const il = props.includedLetter;
                         il.correctPos = e.target.checked;
+                        // if (il.letters.length > 1) {
+                        //     il.letters = props.includedLetter.letters.slice(0, 1);
+                        // }
                         props.onChange(il);
                     }}
                 >
                     Correct Position
                 </Form.Checkbox>
+                {
+                    (props.includedLetter.correctPos && props.includedLetter.letters.length > 1) &&
+                    <Form.Help color="danger">Only 1 Letter can be in the correct position</Form.Help>
+                }
+
             </Form.Control>
         </Form.Field>
     </>;
+}
+
+const CheckBoxStyle: React.CSSProperties = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'start'
 }
 
 export {IncludedLetters}
